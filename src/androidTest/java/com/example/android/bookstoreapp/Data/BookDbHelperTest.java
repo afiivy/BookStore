353cde9b630efed3b589f7a9155d8fd94178f6a5
afiivy <40328172+afiivy@users.noreply.github.com>
2020-10-17 19:10:41 -0500

    Add files via upload

---
+++ src/androidTest/java/com/example/android/bookstoreapp/Data/BookDbHelperTest.java
@@ -0,0 +1,16 @@
+package com.example.android.bookstoreapp.Data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookDbHelperTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onUpgrade() {
+    }
+}
